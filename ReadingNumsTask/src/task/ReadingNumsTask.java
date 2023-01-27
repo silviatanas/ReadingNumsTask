07ef12129c484891437b53299f36dda510126ad1
silviatanas <93332302+silviatanas@users.noreply.github.com>
2023-01-27 16:51:49 +0200

    Update ReadingNumsTask.java

--- ReadingNumsTask/src/task/ReadingNumsTask.java
+++ ReadingNumsTask/src/task/ReadingNumsTask.java
@@ -12,12 +12,12 @@ public class ReadingNumsTask {
         try {
             boolean fileCreated = file.createNewFile();
             if (fileCreated) {
-                System.out.println("File created successfully!");
+                System.out.println("File created successfully!\nProceeding to read file...");
             } else {
-                System.out.println("File already exists.");
+                System.out.println("File already exists.\nProceeding to read file...");
             }
         } catch (IOException e) {
-            System.out.println("Error in creating file.");
+            System.out.println("Error in creating file.\nEnding program.");
             e.printStackTrace();
         }
 
@@ -26,7 +26,7 @@ public class ReadingNumsTask {
             // If my file path is written correctly this should be fine
             fileData = new BufferedReader(new FileReader(file));
         } catch (FileNotFoundException e) {
-            System.out.println("Error in finding file.");
+            System.out.println("Error in finding file.\nEnding program.");
             e.printStackTrace();
         }
 
@@ -36,12 +36,12 @@ public class ReadingNumsTask {
             readingFile = fileData.readLine();
             // need to close?
         } catch (IOException e) {
-            System.out.println("Error in reading file.");
+            System.out.println("Error in reading file.\nEnding program.");
             e.printStackTrace();
         }
 
-        // String into String array
-        String[] readingFileArr = readingFile.split(";"); // separating the string to arr elements
+        // String into String array, using split
+        String[] readingFileArr = readingFile.split(";");
 
         // Making an actual array of numbers and printing
         int[] numbers = new int[readingFileArr.length];
@@ -50,6 +50,19 @@ public class ReadingNumsTask {
             numbers[i] = Integer.parseInt(readingFileArr[i]);
         }
 
-        System.out.println(Arrays.toString(numbers));
+        // Using hashmap to count occurrences
+        HashMap<Integer, Integer> countingNums = new HashMap<>();
+        for (int j : numsArr) {
+            if (countingNums.containsKey(j)) {
+                countingNums.put(j, countingNums.get(j) + 1);
+            } else {
+                countingNums.put(j, 1);
+            }
+        }
+
+        // Printing final results
+        for (Integer key: countingNums.keySet()) {
+            System.out.format("\nNumber: %-10d Occurs: %-10d", key, countingNums.get(key));
+        }
     }
 }
