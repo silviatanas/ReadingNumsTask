41f1944700a684e05a2f3df1da8ff2d2fc601e38
silviatanas <93332302+silviatanas@users.noreply.github.com>
2023-02-02 23:00:06 +0200

    Update ReadWriteCounter.java

--- ReadingNumsTask/src/readwrite/ReadWriteCounter.java
+++ ReadingNumsTask/src/readwrite/ReadWriteCounter.java
@@ -1,88 +1,15 @@
 package readwrite;
 
-import java.io.*;
-import java.util.Random;
-import java.util.Scanner;
+import writing.WritingRandomInts;
 
-public class ReadWriteCounter {
+import java.io.*;
+public class ReadWriteCounter extends WritingRandomInts {
     public static void main(String[] args) {
 
-        long startTime, endTime, finalTime;
+        long endTime, finalTime;
+        long[] startTime = {0};
 
-        // Filepath user input
-        Scanner inputPath = new Scanner(System.in);
-        System.out.println("Enter file path:");
-        String filePath = inputPath.nextLine();
-
-        File file = new File(filePath);
-        FileWriter writer = null;
-
-        // Inputting file size
-        Scanner sizeInput = new Scanner(System.in);
-        int megabytes;
-
-        while (true) {
-            System.out.println("Enter file size up to 10GB (input is in MB):");
-            if (sizeInput.hasNextInt()) {
-                megabytes = sizeInput.nextInt();
-                if (megabytes <= 10_240) {
-                    break;
-                }
-            } else {
-                System.out.println("It appears you haven't put in a number. Try again.");
-                sizeInput.nextLine();
-            }
-        }
-
-        int bytes = megabytes * 1024 * 1024;
-
-        Scanner selection = new Scanner(System.in);
-        String select;
-
-        // Random ints initialization
-        Random random = new Random();
-        int num;
-
-        // START
-        startTime = System.nanoTime();
-
-        // Creating file, checking if a file already exists, writing/overriding file
-        try {
-            boolean fileCreated = file.createNewFile();
-            if (fileCreated) {
-                System.out.println("File created successfully!\nProceeding to write file...");
-            } else {
-                System.out.println("File already exists.");
-
-                do {
-                    System.out.println("Would you like to override file? (Y/N)");
-                    select = selection.nextLine();
-
-                    if (select.equalsIgnoreCase("n")) {
-                        System.out.println("Ending program.");
-                        System.exit(0);
-                    }
-                } while (!select.equalsIgnoreCase("y"));
-
-                System.out.println("Proceeding to write into file...");
-
-                writer = new FileWriter(file);
-
-                // Filling file with random ints separated by ;
-                while (file.length() <= bytes) {
-                    num = random.nextInt();
-                    try {
-                        writer.write(num + ";");
-                    } catch (IOException e) {
-                        System.out.println("Error in writing in file.\nEnding program.");
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Something sure isn't right.\nEnding program.");
-            e.printStackTrace();
-        }
+        File file = writing(startTime);
 
         System.out.println("--------------------------------------------");
         System.out.println("Creation operations finished");
@@ -144,7 +71,7 @@ public class ReadWriteCounter {
 
         // END
         endTime = System.nanoTime();
-        finalTime = endTime - startTime;
+        finalTime = endTime - startTime[0];
 
         System.out.println("TOTAL TIME: " + finalTime / 1_000_000_000 + " seconds");
     }
